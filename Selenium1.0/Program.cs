c830231f5a1554dbc9696c4e390d2872c9f5f2fe
Ji, Jessie <asaggo@byui.edu>
2018-02-12 12:34:36 -0700

    first commit

--- Selenium1.0/Program.cs
+++ Selenium1.0/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +15,72 @@ namespace Selenium1._0
     {
         static void Main(string[] args)
         {
+
+        }
+        
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.target.com/");
+            Console.WriteLine("Open URL");
+        }
+
+        [Test]
+        public void ExecuteTest_Search()
+        {
+            /*
+            IWebElement element = Properties.driver.FindElement(By.Name("q"));
+            element.SendKeys("Jessie Ji");
+            */
+            
+            Console.WriteLine("Execute Test - Search");
+        }
+
+        [Test]
+        public void ExecuteTest_SignUp()
+        {
+            SetMethods.Click("js-toggleRightNavLg", "Id");
+            SetMethods.Click("rightNav-createAccount", "Id");
+
+
+            SetMethods.EnterText("username", "asaggo", "Id");
+            SetMethods.EnterText("firstname", "jessie", "Id");
+            SetMethods.EnterText("lastname", "ji", "Id");
+            SetMethods.EnterText("password", "1234qwer", "Id");
+            SetMethods.Click("createAccount", "Id");
+            Console.WriteLine("Execute Test - SignUp");
+        }
+        
+        [Test]
+        public void Signup_InputTest()
+        {
+
+            SetMethods.Click("js-toggleRightNavLg", "Id");
+            SetMethods.Click("rightNav-createAccount", "Id");
+
+            try
+            {
+
+            }
+            catch (NoSuchElementException e)
+            {
+
+            }
+
+            SetMethods.Click("js-toggleRightNavLg", "Id");
+            SetMethods.Click("rightNav-createAccount", "Id");
+
+
+            SetMethods.EnterText("username", "asaggo", "Id");
+            SetMethods.EnterText("firstname", "jessie", "Id");
+        }
+        
+        [TearDown]
+        public void CleanUp()
+        {
+            //Properties.driver.Close();
+            Console.WriteLine("Close browser");
         }
     }
 }
---
+++ Selenium1.0/SetMethods.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium1._0
+{
+    class SetMethods
+    {
+        public static void EnterText(string elementName, string value, string elementType)
+        {
+
+            WebDriverWait wait = new WebDriverWait(Properties.driver, TimeSpan.FromSeconds(15));
+            if (elementType.Equals("Id"))
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id(elementName))).SendKeys(value);
+            else if (elementType.Equals("Name"))
+                Properties.driver.FindElement(By.Name(elementName)).SendKeys(value);
+        }
+
+        //click a button, checkbox etc
+        public static void Click(string elementName, string elementType)
+        {
+            WebDriverWait wait = new WebDriverWait(Properties.driver, TimeSpan.FromSeconds(15));
+
+            if (elementType.Equals("Id"))
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id(elementName))).Click();
+            else if (elementType.Equals("Name"))
+                Properties.driver.FindElement(By.Name(elementName)).Click();
+            else if (elementType.Equals("ClassName"))
+                Properties.driver.FindElement(By.ClassName(elementName)).Click();
+        }
+
+        public static void SelectDropDown(string elementName, string value, string elementType)
+        {
+            if (elementType.Equals("Id"))
+                new SelectElement(Properties.driver.FindElement(By.Id(elementName))).SelectByValue(value);
+            else if (elementType.Equals("Name"))
+                new SelectElement(Properties.driver.FindElement(By.Name(elementName))).SelectByValue(value);
+            else if (elementType.Equals("ClassName"))
+                new SelectElement(Properties.driver.FindElement(By.ClassName(elementName))).SelectByValue(value);
+        }
+
+        public static void HoverAndClick(IWebElement elementToHover, IWebElement elementToClick)
+        {
+            Actions action = new Actions(Properties.driver);
+            action.MoveToElement(elementToHover).Click(elementToClick);
+
+        }
+    }
+}
